9db65c9f7374eaf6ade2bc319795a73d837993bb
xwlaning <648613081@qq.com>
2018-09-23 21:33:55 +0800

    演示setUpClass用法

--- test_math_func.py
+++ test_math_func.py
@@ -5,8 +5,17 @@
 
 import unittest
 from math_func import *
+from socket_util import *
 
 class TestMathFunc(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print "setUpClass ..."
+        cls._conn = SocketUtil()
+
+    def setUp(self):
+        print "start test {0}...".format(self._testMethodName)
 
     def test_add(self):
         self.assertEqual(3, add(1, 2))
@@ -19,6 +28,14 @@ class TestMathFunc(unittest.TestCase):
 
     def test_divide(self):
         self.assertEqual(3, divide(7, 2))
+
+    def tearDown(self):
+        print "finish test."
+
+    @classmethod
+    def tearDownClass(cls):
+        print "tearDownClass ..."
+        cls._conn.close_sock()
 
 
 if __name__ == "__main__":
