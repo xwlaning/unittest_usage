f2ebe396bd9e6bf94426e6fec0b93c987142f215
xwlaning <648613081@qq.com>
2018-09-23 13:32:05 +0800

    增加演示setUpClass用的socket功能源码

---
+++ socket_util.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# @Author  : xwlan
+# @Time    : 2018/9/23 12:12
+
+class SocketUtil():
+    def __init__(self):
+        print "SocketUtil init"
+
+    def open_sock(self):
+        print "socket opened"
+
+    def close_sock(self):
+        print "socket closed"
+
